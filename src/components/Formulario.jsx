e78a3d939af82357065b4df10247f1118cac2dbe
danieldamian09 <humbertorivero9@gmail.com>
2022-04-15 13:01:04 -0300

    add inputs form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Formulario = () => {
-  return (
-    <div>Formulario</div>
-  )
-}
+	return (
+		<div className="contenedor">
+			<form>
+				<div className="campo">
+					<label htmlFor="ciudad">Ciudad</label>
+					<input type="text" name="ciudad" id="ciudad" />
+				</div>
 
-export default Formulario
+				<div className="campo">
+					<label htmlFor="pais">País</label>
+					<select name="pais" id="pais">
+						<option value="">Seleccione un País</option>
+						<option value="US">Estados Unidos</option>
+						<option value="AR">Argentina</option>
+						<option value="CO">Colombia</option>
+						<option value="CR">Costa Rica</option>
+						<option value="ES">España</option>
+						<option value="PE">Perú</option>
+						<option value="MX">México</option>
+						<option value="VE">Venezuela</option>
+					</select>
+				</div>
+
+				<input type="submit" value="Consultar Clima" />
+			</form>
+		</div>
+	);
+};
+
+export default Formulario;
