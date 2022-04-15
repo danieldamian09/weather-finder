b0c38ea8d819ed6f2e6ab08565a48bd7027962c5
danieldamian09 <humbertorivero9@gmail.com>
2022-04-15 13:41:23 -0300

    reading inputs and validate inputs

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import { useState } from 'react';
+import useClima from "../hooks/useClima";
 
 const Formulario = () => {
+
+	const [alerta, setAlerta] = useState("");
+
+	const { busqueda, datosBusqueda } = useClima();
+	
+	const { ciudad, pais } = busqueda
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		// Validaciones
+		if (Object.values(busqueda).includes("")) {
+			setAlerta("Todos los campos son obligatorios");
+			return
+		}
+		setAlerta("");
+	}
+
 	return (
 		<div className="contenedor">
-			<form>
+			{alerta && <p className="alerta">{alerta}</p>}
+			<form
+				onSubmit={handleSubmit}
+			>
 				<div className="campo">
 					<label htmlFor="ciudad">Ciudad</label>
-					<input type="text" name="ciudad" id="ciudad" />
+					<input type="text" name="ciudad" id="ciudad" onChange={e => datosBusqueda(e)} value={ciudad} />
 				</div>
 
 				<div className="campo">
 					<label htmlFor="pais">País</label>
-					<select name="pais" id="pais">
+					<select name="pais" id="pais" onChange={e => datosBusqueda(e)} value={pais}>
 						<option value="">Seleccione un País</option>
 						<option value="US">Estados Unidos</option>
 						<option value="AR">Argentina</option>
